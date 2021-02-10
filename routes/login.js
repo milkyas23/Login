981c9ef73301c20d6d34b92e0c19ab92659e0eda
milkyas23 <milkyaseyob.habte@elev.ga.ntig.se>
2021-02-10 15:23:11 +0100

    login

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET login form */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('respond with a resource');
+  res.render('login', {title: 'Schoolsoft'});
 });
+
+/* POST login */
+router.post('/', function(req, res, next) {
+
+  console.log(req.body);
+
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (password == "Secret123") {
+    // kod för att kolla uppgifter med db
+    // om login rätt sätt session
+    res.send('Topsecret!');
+  } else {
+    // kommentera ut vid felsökning
+    res.render(
+      'login',
+      {
+        title: 'Schoolsoft',
+        error: 'FEL!'
+      }
+    );
+  }
+
+
+});
+
 
 module.exports = router;
