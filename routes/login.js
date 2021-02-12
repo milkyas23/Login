80b43f15224662e9d8d713fc4d8e660b5fc0e006
milkyas23 <milkyaseyob.habte@elev.ga.ntig.se>
2021-02-12 11:07:37 +0100

    success

--- routes/login.js
+++ routes/login.js
@@ -25,33 +25,64 @@ router.get('/kryptan/:pwd', function(req, res, next) {
 
 
 /* POST login */
-router.post('/', function(req, res, next) {
+router.post('/', async function(req, res, next) {
 
   console.log(req.body);
   
 
   const username = req.body.username;
   const password = req.body.password;
+  if (username && password){
+    try{
+      const sql = 'SELECT password FROM users WHERE name = ?';
+      const result= await query(sql, username);
 
-  if (password == "Secret123") {
-    // kod för att kolla uppgifter med db
-    // om login rätt sätt session
+      if (result.length > 0){
+      bcrypt.compare(password, result[0].password, function(error, result){
+        console.log(result)
+        if (result = true){
+          req.session.loggedin = true;
+          req.session.username = username;
+          res.redirect('/topsecret');
+        } else {
+          res.render('login',{error: 'wrong username or password!'});
+        }
+      });
+    }else {
+      res.render('login',{error: 'wrong username or password!'});
+    }
    
-    res.render('/topsecret');
-
-  } else {
-    // kommentera ut vid felsökning
-    res.render(
-      'login',
-      {
-        title: 'Schoolsoft',
-        error: 'FEL!'
+    }catch(e){
+        next(e);
+        console.error(e);
+    }
+      }else {
+        res.render('login',{error: 'wrong username or password!'});
       }
-    );
-  }
+    
+  });
 
 
-});
+
+//if (password == "Secret123") {
+//     // kod för att kolla uppgifter med db
+//     // om login rätt sätt session
+   
+//     res.render('/topsecret');
+
+//   } else {
+//     // kommentera ut vid felsökning
+//     res.render(
+//       'login',
+//       {
+//         title: 'Schoolsoft',
+//         error: 'FEL!'
+//       }
+//     );
+//   }
+
+
+
 
 
 module.exports = router;
