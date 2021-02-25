6abf524c67af7497a73062e13a426da12570008e
milkyas23 <milkyaseyob.habte@elev.ga.ntig.se>
2021-02-25 10:04:52 +0100

    Skapade register,registertest,controlles,Authcontroller och ändradde app.js

--- app.js
+++ app.js
@@ -5,16 +5,15 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const sassMiddleware = require('node-sass-middleware');
-const session = require ('express-session')
+const session = require('express-session');
 
 const indexRouter = require('./routes/index');
 const loginRouter = require('./routes/login');
 const homeRouter = require('./routes/home');
 const registerRouter = require('./routes/register');
 
-var app = express();
+const app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -25,22 +24,21 @@ app.use(cookieParser());
 app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
+  indentedSyntax: false, // true = .sass and false = .scss
   sourceMap: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(session({
-  secret:'s3KuRT4S',
+  secret: 'S3KuR|T4S', // recommended but wont be set due to http
   resave: false,
   saveUninitialized: true,
-  cookie: {samesite:true}
-}))
+  cookie: {samesite: true}
+}));
 
 app.use('/', indexRouter);
 app.use('/login', loginRouter);
-app.use('/register', registerRouter);
 app.use('/home', homeRouter);
+app.use('/register', registerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/register.js
+++ routes/register.js
@@ -1,13 +1,24 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { body, validationResult } = require('express-validator');
+const registercontroller = require('../controllers/Registercontroller');
+
+/* GET Login form. */
+router.get('/', registercontroller.show);
 
 /* GET topsecret page. */
-router.get('/topsecret', function(req, res, next) {
-  if (req.session.loggedin) {
-    res.send('Du är inlogad'); 
-  } else{
-  res.render('please login to view this page!');
-  }
-});
+router.post('/',
+  body('username').notEmpty().trim().toLowerCase(),
+  body('email').notEmpty().isEmail().trim().toLowerCase(),
+  body('password').notEmpty(),
+  body('passwordconfirmation').custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error('Password confirmation does not match password');
+    }
+    // Indicates the success of this synchronous custom validator
+    return true;
+  }),
+  registercontroller.store
+);
 
 module.exports = router;
